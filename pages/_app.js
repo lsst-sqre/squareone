c9b8af854686bcc0a2c42aa5ae0cd645d7af5aec
Jonathan Sick <jsick@lsst.org>
2021-01-07 13:51:29 -0500

    Initial linting with to wesbos's eslint rules

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import PropTypes from 'prop-types';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  /* eslint-disable react/jsx-props-no-spreading */
+  return <Component {...pageProps} />;
+  /* eslint-enable react/jsx-props-no-spreading */
 }
 
-export default MyApp
+export default MyApp;
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
