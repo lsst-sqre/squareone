bc4a2b2b68f61fa6c45db204bf199f62b48044d8
Jonathan Sick <jsick@lsst.org>
2021-03-03 12:28:01 -0500

    Implement a sticky-footer
    See https://css-tricks.com/couple-takes-sticky-footer/

--- components/page.js
+++ components/page.js
@@ -1,8 +1,31 @@
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import Header from './header';
 import MainContent from './mainContent';
 import Footer from './footer';
+
+/*
+ * Layout wrapper div.
+ *
+ * Its main job is to provide a "sticky footer" so that the Footer component
+ * stays at the bottom of the page and the Header/MainContent components
+ * take up any excess space. See
+ * https://css-tricks.com/couple-takes-sticky-footer/
+ */
+const StyledLayout = styled.div`
+  /* Flexbox for the sticky footer */
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+
+  .upper-container {
+    flex: 1 0 auto;
+  }
+  .sticky-footer-container {
+    flex-shink: 0;
+  }
+`;
 
 /*
  * Page wapper component that provides the default layout of navigation,
@@ -10,11 +33,15 @@ import Footer from './footer';
  */
 export default function Page({ children }) {
   return (
-    <>
-      <Header />
-      <MainContent>{children}</MainContent>
-      <Footer />
-    </>
+    <StyledLayout>
+      <div className="upper-container">
+        <Header />
+        <MainContent>{children}</MainContent>
+      </div>
+      <div className="sticky-footer-container">
+        <Footer />
+      </div>
+    </StyledLayout>
   );
 }
 
