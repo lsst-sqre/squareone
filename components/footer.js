0195b501b96d96670103e0e3b419942743555f50
Jonathan Sick <jsick@lsst.org>
2021-03-09 17:15:49 -0500

    Apply basic page layout to content and footer

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,18 @@
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+  margin: 0 auto;
+  max-width: 60em;
+  padding: 0 10px 0 10px;
+`;
+
 /*
  * Footer component (contained within a Page component).
  */
 export default function Footer() {
   return (
-    <footer>
+    <StyledFooter>
       <p>Footer</p>
-    </footer>
+    </StyledFooter>
   );
 }
--- components/mainContent.js
+++ components/mainContent.js
@@ -1,10 +1,17 @@
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledMain = styled.main`
+  margin: 0 auto;
+  max-width: 60em;
+  padding: 0 10px 0 10px;
+`;
 
 /*
  * Main content wrapper (contained within a Page component).
  */
 export default function MainContent({ children }) {
-  return <main>{children}</main>;
+  return <StyledMain>{children}</StyledMain>;
 }
 
 MainContent.propTypes = {
