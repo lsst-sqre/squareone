80726840ac60a96023815d9072bf5e5311d36c25
Jonathan Sick <jsick@lsst.org>
2021-03-10 17:17:21 -0500

    Add lang and dir to html tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 /*
@@ -34,5 +34,17 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html lang="en" dir="ltr">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
