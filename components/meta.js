f9698a4cfcd031d7f5d7b7846777d53588ca134d
Jonathan Sick <jsick@lsst.org>
2021-03-11 13:27:04 -0500

    Add site description metadata

--- components/meta.js
+++ components/meta.js
@@ -1,25 +1,40 @@
 import Head from 'next/head';
+import getConfig from 'next/config';
 
-const Meta = () => (
-  <Head>
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1"
-      key="viewport"
-    />
-    <meta charSet="utf-8" key="viewport" />
-    <link
-      rel="icon"
-      type="image/svg+xml"
-      href="/rubin-favicon.svg"
-      key="favicon"
-    />
-    <link
-      rel="alternate icon"
-      href="/rubin-favicon-transparent-32px.png"
-      key="altfavicon"
-    />
-  </Head>
-);
+const Meta = () => {
+  const { publicRuntimeConfig } = getConfig();
+  const { siteTitle, siteDescription } = publicRuntimeConfig;
+
+  return (
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+        key="viewport"
+      />
+      <meta charSet="utf-8" key="viewport" />
+      <link
+        rel="icon"
+        type="image/svg+xml"
+        href="/rubin-favicon.svg"
+        key="favicon"
+      />
+      <link
+        rel="alternate icon"
+        href="/rubin-favicon-transparent-32px.png"
+        key="altfavicon"
+      />
+      <title key="title">{siteTitle}</title>
+      <meta name="description" key="description" content={siteDescription} />
+
+      <meta property="og:title" key="ogtitle" content={siteTitle} />
+      <meta
+        property="og:description"
+        key="ogdescription"
+        content={siteDescription}
+      />
+    </Head>
+  );
+};
 
 export default Meta;
