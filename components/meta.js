6cccdccbc44c971b402be9fd258245e390ce8707
Jonathan Sick <jsick@lsst.org>
2021-03-15 12:11:12 -0400

    Use Source Sans Pro from Adobe fonts
    This is the body font specified by our Visual Identity Manual. Right now
    I'm using Google Fonts to host the fonts (400, 400 italic and 700).
    Eventually we'll want to host it ourselves and do some subsetting to
    optimize page loading.
    
    https://fonts.google.com/specimen/Source+Sans+Pro?preview.text_type=custom#standard-styles

--- components/meta.js
+++ components/meta.js
@@ -33,6 +33,12 @@ const Meta = () => {
         key="ogdescription"
         content={siteDescription}
       />
+
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400&display=swap"
+        rel="stylesheet"
+      />
     </Head>
   );
 };
