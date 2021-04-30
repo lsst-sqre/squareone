51d40fc9062ac1a638f6627abfef71d0dfc35869
Jonathan Sick <jsick@lsst.org>
2021-04-30 12:19:35 -0400

    Load Source Sans Pro via Font Source

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,22 @@ import PropTypes from 'prop-types';
 import getConfig from 'next/config';
 import { ThemeProvider } from 'next-themes';
 
+// Icons from Font Awesome
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
+// Source Sans Pro Font from Font Source
+import '@fontsource/source-sans-pro/400.css';
+import '@fontsource/source-sans-pro/400-italic.css';
+import '@fontsource/source-sans-pro/700.css';
+
+// Global CSS
 import 'normalize.css';
 import '@lsst-sqre/rubin-style-dictionary/dist/tokens.css';
 import '@lsst-sqre/rubin-style-dictionary/dist/tokens.dark.css';
 import '../styles/globals.css';
+
 import { useLogin } from '../hooks/login';
 import Page from '../components/page';
 
