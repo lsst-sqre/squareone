dba61faca9bc3b6b1a5d1ca2d49e22b6e9a95beb
Jonathan Sick <jsick@lsst.org>
2021-06-23 16:13:57 -0400

    Write copy and style the /support page

--- components/typography.js
+++ components/typography.js
@@ -2,4 +2,23 @@ import styled from 'styled-components';
 
 export const Lede = styled.p`
   font-size: 1.3rem;
+  margin-bottom: 2rem;
+`;
+
+export const CtaLink = styled.a`
+  display: inline-block;
+  font-size: 1.3rem;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  --tw-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  --tw-ring-offset-shadow: 0 0 transparent;
+  --tw-ring-shadow: 0 0 transparent;
+  box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000),
+    var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+  border: solid 1px transparent;
+  background-color: var(--sqo-doc-card-background-color);
+
+  &:hover {
+    border: solid 1px var(--rsd-color-primary-600);
+  }
 `;
--- pages/support.js
+++ pages/support.js
@@ -1,6 +1,14 @@
 import Head from 'next/head';
 import getConfig from 'next/config';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
+import styled from 'styled-components';
+
+import { Lede, CtaLink } from '../components/typography';
+
+const Section = styled.section`
+  margin-bottom: 2rem;
+`;
 
 const pageDescription =
   'Get help with the Rubin Science Platform, data, and software.';
@@ -21,7 +29,45 @@ export default function SupportPage({ publicRuntimeConfig }) {
 
       <h1>Get help with the Rubin Science Platform</h1>
 
-      <p>TK</p>
+      <Lede>
+        Besides the <Link href="/docs">documentation</Link>, you can get help
+        from Rubin Observatory staff. Here are the ways to ask for help.
+      </Lede>
+
+      <Section>
+        <h2>Data Preview 0 science questions</h2>
+
+        <p>
+          For questions about the Data Preview dataset (DESC DC2) and analyzing
+          that data (such as with the LSST Science Pipelines), create a new
+          topic in the{' '}
+          <a href="https://community.lsst.org/c/support/dp0/49">
+            Data Preview 0 Support category
+          </a>{' '}
+          of the Community forum.
+        </p>
+
+        <CtaLink
+          href="http://community.lsst.org/new-topic?category=support/dp0
+        "
+        >
+          Create a science support topic in the forum.
+        </CtaLink>
+      </Section>
+
+      <Section>
+        <h2>Rubin Science Platform technical support and feature requests</h2>
+        <p>
+          For technical issues or feature requests related to the Rubin Science
+          Platform itself (the Portal, Notebooks, and API services such as TAP)
+          create a GitHub issue in the{' '}
+          <a href="https://github.com/rubin-dp0/Support">dp0-1/Support</a>{' '}
+          repository.
+        </p>
+        <CtaLink href="https://github.com/rubin-dp0/Support/issues/new/choose">
+          Create a GitHub issue
+        </CtaLink>
+      </Section>
     </>
   );
 }
