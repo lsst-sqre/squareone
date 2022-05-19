fd8e6089d8ac6c8ac43733d148151d3f711c3529
Jonathan Sick <jsick@lsst.org>
2022-05-19 18:05:20 -0400

    Remove original /times-square/nb URLs
    The /nb/ URLs were the original prototype pages; but have now been
    replaced by /github/:path pages and (to-be-added) /u/:username/:name
    pages for interactive uploads.
    
    Also remove the original page listing via the Times Square /v1/pages
    endpoint; that's replaced by the GitHubContentsTree component that shows
    contents of github-backed pages.

--- src/pages/times-square/index.js
+++ src/pages/times-square/index.js
@@ -3,51 +3,24 @@ import getConfig from 'next/config';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 
-import useSWR from 'swr';
-
 import TimesSquareApp from '../../components/TimesSquareApp';
 import WideContentLayout from '../../components/WideContentLayout';
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function TimesSquareHome({ publicRuntimeConfig }) {
-  const { timesSquareUrl } = publicRuntimeConfig;
-  const pagesDataUrl = `${timesSquareUrl}/v1/pages`;
-
-  const { data: pageResources, error } = useSWR(pagesDataUrl, fetcher, {});
-
-  if (pageResources) {
-    return (
-      <TimesSquareApp>
-        <Head>
-          <title>Times Square | {publicRuntimeConfig.siteName}</title>
-        </Head>
-        <h1>Times Square</h1>
-        <p>
-          View Jupyter Notebooks on the Rubin Science Platform, computed
-          on-demand with configurable parameters.
-        </p>
-        <h2>Pages</h2>
-        <ul>
-          {pageResources.map((page) => (
-            <li key={page.name}>
-              <Link href={`/times-square/nb/${page.name}`}>{page.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </TimesSquareApp>
-    );
-  } else {
-    return (
-      <TimesSquareApp>
-        <Head>
-          <title>{publicRuntimeConfig.siteName}</title>
-        </Head>
-        <h1>Times Square</h1>
-        <p>Loading...</p>
-      </TimesSquareApp>
-    );
-  }
+  return (
+    <TimesSquareApp>
+      <Head>
+        <title>Times Square | {publicRuntimeConfig.siteName}</title>
+      </Head>
+      <h1>Times Square</h1>
+      <p>
+        View Jupyter Notebooks on the Rubin Science Platform, computed on-demand
+        with configurable parameters.
+      </p>
+    </TimesSquareApp>
+  );
 }
 
 TimesSquareHome.propTypes = {
