9b1abd451feaef33c1fc06e59a33cd03ab984112
Jonathan Sick <jsick@lsst.org>
2022-05-30 16:38:19 -0400

    Demonstrate form validation with ajv and Formik
    This shows how to write a validate callback function for Formik that
    validates parameters based on the JSON Schema definitions of each
    parameter.
    
    The validation messages are returned to the UI.

--- src/components/TimesSquareParameters/TimesSquareParameters.js
+++ src/components/TimesSquareParameters/TimesSquareParameters.js
@@ -1,10 +1,22 @@
 import { useRouter } from 'next/router';
 import { Formik } from 'formik';
+import Ajv from 'ajv';
 
 export default function TimesSquareParameters({ pageData, userParameters }) {
   const router = useRouter();
   const { parameters } = pageData;
+  const ajv = new Ajv({ coerceTypes: true });
 
+  // Prepare executable validators for each parameter from their
+  // JSON schema definitions.
+  const schemas = {};
+  Object.entries(parameters).forEach(
+    ([paramName, paramSchemaDef]) =>
+      (schemas[paramName] = ajv.compile(paramSchemaDef))
+  );
+
+  // Callback function to handle form submission by "navigating"
+  // to a new set of query parameters.
   const handleFormSubmit = (values, { setSubmitting }) => {
     // 1. Get an object with the existing query from the router. This gives us
     // any existing path parameters for dynamic routing or misc query parameters
@@ -23,9 +35,35 @@ export default function TimesSquareParameters({ pageData, userParameters }) {
     setSubmitting(false);
   };
 
+  // Callback function to handle  form validation.
+  const validate = (values) => {
+    const errors = {};
+    Object.entries(values).forEach(([paramName, value]) => {
+      const valid = schemas[paramName](value);
+      if (!valid) {
+        const error_messages = schemas[paramName].errors.map(
+          (error) => error.message
+        );
+        errors[paramName] = error_messages.join('. ');
+      }
+    });
+    return errors;
+  };
+
   return (
-    <Formik initialValues={userParameters} onSubmit={handleFormSubmit}>
-      {({ values, handleChange, handleSubmit, isSubmitting }) => (
+    <Formik
+      initialValues={userParameters}
+      onSubmit={handleFormSubmit}
+      validate={validate}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleSubmit,
+        isSubmitting,
+      }) => (
         <form onSubmit={handleSubmit}>
           <ul>
             {Object.entries(parameters).map((item) => {
@@ -41,6 +79,9 @@ export default function TimesSquareParameters({ pageData, userParameters }) {
                       value={values[paramName]}
                       onChange={handleChange}
                     />
+                    {errors[paramName] && touched[paramName] && (
+                      <div>{errors[paramName]}</div>
+                    )}
                   </label>
                 </li>
               );
