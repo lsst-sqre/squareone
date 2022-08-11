c28d5848fa56d6eb7bfe3bb4e1961a875dae1752
Jonathan Sick <jsick@lsst.org>
2022-08-11 16:44:25 -0400

    Add a story for the homepage
    This story is missing a bunch of CSS --- I'll have to figure out why the
    backgrounds on the individual aspects aren't showing up.

---
+++ src/stories/pages/home.stories.js
@@ -0,0 +1,13 @@
+import Home from '../../pages/index';
+
+export default {
+  title: 'Pages/Homepage',
+  component: Home,
+};
+
+const Template = (args) => <Home {...args} />;
+
+export const Homepage = Template.bind();
+Homepage.args = {
+  publicRuntimeConfig: { siteName: 'Rubin Science Platform' },
+};
