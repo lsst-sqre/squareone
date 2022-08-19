a43520cdb010c79caff64fe57fd46b35b6b1d276
Jonathan Sick <jsick@lsst.org>
2022-08-19 13:42:47 -0400

    Create stories for TimesSquareGitHubNav

---
+++ src/components/TimesSquareGitHubNav/TimesSquareGitHubNav.stories.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import TimesSquareGitHubNav from './TimesSquareGitHubNav';
+
+export default {
+  component: TimesSquareGitHubNav,
+  title: 'Components/TimesSquare/GitHubNav',
+};
+
+const Template = (args) => <TimesSquareGitHubNav {...args} />;
+
+const exampleGitHubContents = [
+  {
+    node_type: 'owner',
+    path: 'lsst-sqre',
+    title: 'lsst-sqre',
+    contents: [
+      {
+        node_type: 'repo',
+        path: 'lsst-sqre/times-square-demo',
+        title: 'times-square-demo',
+        contents: [
+          {
+            node_type: 'page',
+            path: 'lsst-sqre/times-square-demo/demo',
+            title: 'Sine wave',
+            contents: [],
+          },
+          {
+            node_type: 'directory',
+            path: 'lsst-sqre/times-square-demo/matplotlib',
+            title: 'matplotlib',
+            contents: [
+              {
+                node_type: 'page',
+                path: 'lsst-sqre/times-square-demo/matplotlib/gaussian2d',
+                title: 'Gaussian 2D',
+                contents: [],
+              },
+            ],
+          },
+          {
+            node_type: 'directory',
+            path: 'lsst-sqre/times-square-demo/nightly',
+            title: 'nightly',
+            contents: [
+              {
+                node_type: 'page',
+                path: 'lsst-sqre/times-square-demo/nightly/auxtel',
+                title: 'AuxTel Nightly Report',
+                contents: [],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export const Default = Template.bind({});
+Default.args = {
+  contentNodes: exampleGitHubContents,
+  pagePath: '',
+  pagePathRoot: '/times-square/github',
+};
+
+export const ActivePage = Template.bind({});
+ActivePage.args = {
+  contentNodes: exampleGitHubContents,
+  pagePath: 'lsst-sqre/times-square-demo/matplotlib/gaussian2d',
+  pagePathRoot: '/times-square/github',
+};
