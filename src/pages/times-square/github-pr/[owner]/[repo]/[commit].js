98838e0909b0174fa7269cfaa20cf41d359e7e6c
Jonathan Sick <jsick@lsst.org>
2022-08-25 12:53:00 -0400

    Stylize the commit preview homepage

--- src/pages/times-square/github-pr/[owner]/[repo]/[commit].js
+++ src/pages/times-square/github-pr/[owner]/[repo]/[commit].js
@@ -1,12 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 import Head from 'next/head';
 import getConfig from 'next/config';
 import { useRouter } from 'next/router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import TimesSquareApp from '../../../../../components/TimesSquareApp';
 import TimesSquarePrGitHubNav from '../../../../../components/TimesSquarePrGitHubNav';
 import WideContentLayout from '../../../../../components/WideContentLayout';
 import useGitHubPrContentsListing from '../../../../../components/TimesSquarePrGitHubNav/useGitHubPrContentsListing';
+import GitHubCheckBadge from '../../../../../components/TimesSquarePrGitHubNav/GitHubCheckBadge';
+import GitHubPrBadge from '../../../../../components/TimesSquarePrGitHubNav/GitHubPrBadge';
 
 export default function GitHubPrLandingPage({}) {
   const { publicRuntimeConfig } = getConfig();
@@ -35,18 +39,44 @@ export default function GitHubPrLandingPage({}) {
     const { nbCheck, yamlCheck } = githubContents;
     prDetails = (
       <>
-        {githubContents.pullRequests.map((pr) => (
-          <p key={`pr-${pr.number}`}>
-            <a
-              href={pr.conversation_url}
-            >{`PR#${pr.number} (${pr.state}) ${pr.title}`}</a>{' '}
-            by <a href={pr.contributor.html_url}>{pr.contributor.username}</a>
-          </p>
-        ))}
-        <p>{`${nbCheck.name}: ${nbCheck.conclusion || nbCheck.status}`}</p>
-        <p>{`${yamlCheck.name}: ${
-          yamlCheck.conclusion || yamlCheck.status
-        }`}</p>
+        <ItemList>
+          {githubContents.pullRequests.map((pr) => (
+            <li key={`pr-${pr.number}`}>
+              <GitHubPrBadge
+                state={pr.state}
+                number={pr.number}
+                url={pr.conversation_url}
+                title={pr.title}
+                authorName={pr.contributor.username}
+                authorAvatarUrl={pr.contributor.avatar_url}
+                authorUrl={pr.contributor.html_url}
+              />
+            </li>
+          ))}
+        </ItemList>
+
+        <ItemList>
+          {nbCheck && (
+            <li>
+              <GitHubCheckBadge
+                status={nbCheck.status}
+                title={nbCheck.report_title}
+                conclusion={nbCheck.conclusion}
+                url={nbCheck.html_url}
+              />
+            </li>
+          )}
+          {yamlCheck && (
+            <li>
+              <GitHubCheckBadge
+                status={yamlCheck.status}
+                title={yamlCheck.report_title}
+                conclusion={yamlCheck.conclusion}
+                url={yamlCheck.html_url}
+              />
+            </li>
+          )}
+        </ItemList>
       </>
     );
   } else {
@@ -62,7 +92,16 @@ export default function GitHubPrLandingPage({}) {
         </title>
       </Head>
 
-      <h1>Pull Request Preview for {`${owner}/${repo} at ${commit}`}</h1>
+      <StyledHeader>
+        <p className="subtitle">Pull Request Preview</p>
+        <h1>
+          {`${owner}/${repo}`}{' '}
+          <CommitSpan>
+            <FontAwesomeIcon icon="code-commit" /> {commit.slice(0, 7)}
+          </CommitSpan>
+        </h1>
+      </StyledHeader>
+
       {prDetails}
     </TimesSquareApp>
   );
@@ -83,3 +122,26 @@ export async function getServerSideProps() {
     props: {},
   };
 }
+
+const StyledHeader = styled.header`
+  .subtitle {
+    font-size: 1.2rem;
+    color: var(--rsd-component-text-color);
+    text-transform: uppercase;
+    font-weight: bold;
+  }
+`;
+
+const CommitSpan = styled.span`
+  display: inline-block;
+
+  .commit-icon {
+    margin-right: 0.2rem;
+  }
+`;
+
+const ItemList = styled.ul`
+  list-style: none;
+  margin: 1rem 0 1rem;
+  padding-left: 0;
+`;
