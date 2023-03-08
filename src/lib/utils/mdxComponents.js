122a2f468f41fc50e90622084a825516013b3228
Jonathan Sick <jsick@lsst.org>
2023-03-08 14:39:53 -0500

    Make /support page configurable with MDX
    Set the content of the support page via the supportMdx configuration in
    squareone.config.yaml. This content is rendered into JSX server side.
    Some custom components can be used to write the MDX, such as Link, Lede,
    Section and the CtaLink button. More can be added later. These are
    centrally-mainained in a mdxComponents utils module.
    
    Overally this approach gives deployers full control over the support
    page with Kubernetes-level configuration.

---
+++ src/lib/utils/mdxComponents.js
@@ -0,0 +1,8 @@
+/**
+ * Common sets of React components to use with MDXRemote.
+ */
+
+import Link from 'next/link';
+import { Lede, CtaLink } from '../../components/Typography';
+
+export const commonMdxComponents = { Link, Lede, CtaLink };
