542274b3a26623eac32cc06d559de3127ab452c1
Jonathan Sick <jsick@lsst.org>
2023-08-02 13:22:55 -0400

    Simplify lintstaged script

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,6 @@
 const path = require('path');
 
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
-
 module.exports = {
-  // Run js files through next lint
-  // FIXME this is not working as a monorepo
-  // '*.{js,jsx,ts,tsx}': ['prettier --write', buildEslintCommand],
   '*.{js,jsx,ts,tsx}': ['prettier --write'],
   '*.{yaml,yml,json}': ['prettier --write'],
 };
