12290eb9a602e77783c9b02a41daf4fc03b5cf4d
Jonathan Sick <jsick@lsst.org>
2023-08-03 13:26:03 -0400

    Directory copy favicons into the squareone public/
    We can't use symlinks with the turbo-based build, and we also can't
    import the favicons from the rubin-style-dictionary package --- that
    doesn't work with the standard link element. Instead, just copy the
    assets for now.

--- apps/squareone/src/components/Meta/Meta.js
+++ apps/squareone/src/components/Meta/Meta.js
@@ -1,9 +1,6 @@
 import Head from 'next/head';
 import getConfig from 'next/config';
 
-import SvgFavicon from '@lsst-sqre/rubin-style-dictionary/assets/favicon/rubin-favicon.svg';
-import PngFavicon from '@lsst-sqre/rubin-style-dictionary/assets/favicon/rubin-favicon-transparent-32px.png';
-
 const Meta = () => {
   const { publicRuntimeConfig } = getConfig();
   const { siteName, siteDescription } = publicRuntimeConfig;
@@ -16,8 +13,17 @@ const Meta = () => {
         key="viewport"
       />
       <meta charSet="utf-8" key="charset" />
-      <link rel="icon" type="image/svg+xml" href={SvgFavicon} key="favicon" />
-      <link rel="alternate icon" href={PngFavicon} key="altfavicon" />
+      <link
+        rel="icon"
+        type="image/svg+xml"
+        href="/rubin-favicon.svg"
+        key="favicon"
+      />
+      <link
+        rel="alternate icon"
+        href="/rubin-favicon-transparent-32px.png"
+        key="altfavicon"
+      />
       <title key="title">{siteName}</title>
       <meta name="description" key="description" content={siteDescription} />
 
