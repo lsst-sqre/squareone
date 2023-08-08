715ba245afc46a90a39ec4eb478786c76ac6a6e2
Jonathan Sick <jsick@lsst.org>
2023-08-08 19:19:30 -0400

    Enable light/dark theme in storybook
    This puts a light/dark theme switcher in the storybook view. It
    simulates the effect of changing data-theme attribute, much like
    next-themes does.
    
    I'm not sure this is the ultimate solution. Ideally we want to be able
    to control the background of a component at the same time as we toggle
    the theme. Or be able to show both light and dark themes at the same
    time.

--- packages/squared/.storybook/preview.ts
+++ packages/squared/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react';
+import { withThemeByDataAttribute } from '@storybook/addon-styling';
 
 const preview: Preview = {
   parameters: {
@@ -13,3 +14,18 @@ const preview: Preview = {
 };
 
 export default preview;
+
+// Match the behaviour of next-themes, https://github.com/pacocoursey/next-themes
+// It could also be possible to use withThemeFromJSXProvider to use the
+// ThemeProvider directly; see
+// https://github.com/storybookjs/addon-styling/blob/next/docs/api.md#withthemefromjsxprovider
+export const decorators = [
+  withThemeByDataAttribute({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+    attributeName: 'data-theme',
+  }),
+];
