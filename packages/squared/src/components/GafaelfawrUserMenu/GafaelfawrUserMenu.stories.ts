7b9d174b64153a6bb6f7695bc59a3430d2beb8b9
Jonathan Sick <jsick@lsst.org>
2023-08-14 15:29:03 -0400

    Mock up being logged in/out in stories

--- packages/squared/src/components/GafaelfawrUserMenu/GafaelfawrUserMenu.stories.ts
+++ packages/squared/src/components/GafaelfawrUserMenu/GafaelfawrUserMenu.stories.ts
@@ -11,5 +11,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: { children: 'User Menu' },
+  args: { children: 'User Menu', loggedIn: true },
+};
+
+export const LoggedOut: Story = {
+  args: { children: 'User Menu', loggedIn: false },
 };
--- packages/squared/src/components/GafaelfawrUserMenu/GafaelfawrUserMenu.tsx
+++ packages/squared/src/components/GafaelfawrUserMenu/GafaelfawrUserMenu.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 
 export interface GafaelfawrUserMenuProps {
   children: React.ReactNode;
+  loggedIn: boolean; // temp shim for useGafaelfawrUser
 }
 
-export const GafaelfawrUserMenu = ({ children }: GafaelfawrUserMenuProps) => {
-  const hi = 'Hello world';
-  return <p>{hi}</p>;
+export const GafaelfawrUserMenu = ({
+  children,
+  loggedIn,
+}: GafaelfawrUserMenuProps) => {
+  if (loggedIn) {
+    return <p>Log out</p>;
+  } else {
+    return <p>Log in / Sign up</p>;
+  }
 };
 
 export default GafaelfawrUserMenu;
