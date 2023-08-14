b765732db52e354026294fce7b5ef7c32d32e553
Jonathan Sick <jsick@lsst.org>
2023-08-14 14:54:16 -0400

    Add useGafaelfawrUser hook
    This hook leverages swr to get a user's data from Gafaelfawr's
    /auth/api/v1/user-info endpoint.
    
    New dependencies:
    
    - swr
    - unfetch as a polyfill for fetch; this is needed because squared isn't
      inside a Next.js project that bakes the polyfill in for us.

---
+++ packages/squared/src/index.ts
@@ -0,0 +1,8 @@
+/* Components */
+export { Button, type ButtonProps } from './Button';
+
+/* Hooks */
+export {
+  default as useGafaelfawrUser,
+  type GafaelfawrUser,
+} from './hooks/useGafaelfawrUser';
