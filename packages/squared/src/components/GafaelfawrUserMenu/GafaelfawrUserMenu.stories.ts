13079534ffebc23c745bfc6d2835b6a23056e7a6
Jonathan Sick <jsick@lsst.org>
2023-08-15 11:00:32 -0400

    Add loginHref prop to GafaelfawrUserMenu

--- packages/squared/src/components/GafaelfawrUserMenu/GafaelfawrUserMenu.stories.ts
+++ packages/squared/src/components/GafaelfawrUserMenu/GafaelfawrUserMenu.stories.ts
@@ -11,9 +11,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: { children: 'User Menu', loggedIn: true },
+  args: { children: 'User Menu', loggedIn: true, loginHref: '/login' },
 };
 
 export const LoggedOut: Story = {
-  args: { children: 'User Menu', loggedIn: false },
+  args: { ...Default.args, children: 'User Menu', loggedIn: false },
 };
--- packages/squared/src/components/GafaelfawrUserMenu/GafaelfawrUserMenu.tsx
+++ packages/squared/src/components/GafaelfawrUserMenu/GafaelfawrUserMenu.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 
 export interface GafaelfawrUserMenuProps {
   children: React.ReactNode;
+  /**
+   * Whether the user is logged in. This is a temporary shim until we have a
+   * proper useGafaelfawrUser hook.
+   */
   loggedIn: boolean; // temp shim for useGafaelfawrUser
+  /**
+   * The URL to which the user should be redirected to log in.
+   */
+  loginHref: string;
 }
 
 export const GafaelfawrUserMenu = ({
   children,
   loggedIn,
+  loginHref,
 }: GafaelfawrUserMenuProps) => {
   if (loggedIn) {
     return <p>Log out</p>;
   } else {
-    return <p>Log in / Sign up</p>;
+    return <a href={loginHref}>Log in / Sign up</a>;
   }
 };
 
