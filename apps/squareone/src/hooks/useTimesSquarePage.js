25d9c27980b23bf5386b13b7bd70aae8660882f7
Jonathan Sick <jsick@lsst.org>
2024-04-11 17:19:49 -0400

    Include github and rendered ipynb URL in page data
    This exposes additional data from the Times Square /v1/pages/:page
    endpoint.

--- apps/squareone/src/hooks/useTimesSquarePage.js
+++ apps/squareone/src/hooks/useTimesSquarePage.js
@@ -9,6 +9,13 @@ function useTimesSquarePage() {
   const { tsPageUrl } = React.useContext(TimesSquareUrlParametersContext);
   const { data, error } = useSWR(tsPageUrl, fetcher);
 
+  const githubInfo = {
+    owner: data.github.owner ? data.github.owner : null,
+    repository: data.github.repository ? data.github.repository : null,
+    sourcePath: data.github.source_path ? data.github.source_path : null,
+    sidecarPath: data.github.sidecar_path ? data.github.sidecar_path : null,
+  };
+
   return {
     error: error,
     loading: !error && !data,
@@ -18,6 +25,8 @@ function useTimesSquarePage() {
     htmlUrl: data ? data.html_url : null,
     htmlStatusUrl: data ? data.html_status_url : null,
     htmlEventsUrl: data ? data.html_events_url : null,
+    renderedIpynbUrl: data ? data.rendered_url : null,
+    github: githubInfo,
   };
 }
 
