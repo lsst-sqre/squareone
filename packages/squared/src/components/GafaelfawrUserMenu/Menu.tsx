06df284ac1ae018f62236ce1f7c57588a04d0f1b
Jonathan Sick <jsick@lsst.org>
2024-05-24 13:10:12 -0400

    Fix styling of the GafaelfawrUserMenu
    - It seems that the a selector on the nav bar is overriding the
      specificity of the color rule on MenuLink, so I had to add important!
      there. This needs to be fixed.
    
      - Adjusted positioning of the drop down menu so it appears on the
        screen.

--- packages/squared/src/components/GafaelfawrUserMenu/Menu.tsx
+++ packages/squared/src/components/GafaelfawrUserMenu/Menu.tsx
@@ -86,7 +86,6 @@ const MenuContent = styled(RadixNavigationMenu.Content)`
   min-width: 12rem;
   border-radius: 0.5rem;
   padding: var(--gafaelfawr-user-menu-padding);
-  color: var(--rsd-component-header-nav-menulist-text-color);
   box-shadow: 0px 10px 38px -10px rgba(22, 23, 24, 0.35),
     0px 10px 20px -15px rgba(22, 23, 24, 0.2);
   animation-duration: 400ms;
@@ -95,12 +94,14 @@ const MenuContent = styled(RadixNavigationMenu.Content)`
 `;
 
 export const MenuLink = styled(RadixNavigationMenu.Link)`
-  color: var(--rsd-component-header-nav-menulist-text-color);
+  /* The styling on the menu triggers is overriding this colour. Need to re-address. */
+  color: var(--rsd-component-header-nav-menulist-text-color) !important;
   border-radius: 0.5rem;
   padding: calc(var(--gafaelfawr-user-menu-padding) / 2)
     var(--gafaelfawr-user-menu-padding);
   margin: calc(var(--gafaelfawr-user-menu-padding) / -2);
   margin-bottom: calc(var(--gafaelfawr-user-menu-padding) / 2);
+
   &:last-of-type {
     margin-bottom: 0;
   }
@@ -116,35 +117,24 @@ export const MenuLink = styled(RadixNavigationMenu.Link)`
     background-color: var(
       --rsd-component-header-nav-menulist-selected-background-color
     );
-    color: white;
-
-    a {
-      color: white;
-    }
+    color: white !important;
   }
 `;
 
 const ContentViewport = styled(RadixNavigationMenu.Viewport)`
-  // position: relative;
-  // transform-origin: top center;
-  // margin-top: 10px;
-  // width: 100%;
-  // background-color: white;
-  // border-radius: 6px;
-  // overflow: hidden;
-  // box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px,
-  //   hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
-  // height: var(--radix-navigation-menu-viewport-height);
+  /* This pushes the menu leftward to stay on the page.
+     It's a magic number. Perhaps we can use the pushover API to
+     position the menu more accurately?
+  */
+  position: relative;
+  right: 3rem;
+  height: var(--radix-navigation-menu-viewport-height);
+  width: var(--radix-navigation-menu-viewport-width);
 `;
 
 const ViewportContainer = styled.div`
   position: absolute;
-  // display: flex;
-  // justify-content: center;
-  // width: 100%;
-  // top: 100%;
-  // left: 0;
-  // perspective: 2000px;
+  z-index: 1000;
 `;
 
 export default Menu;
