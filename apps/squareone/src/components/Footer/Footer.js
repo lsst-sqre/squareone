0cf830d34899aeee56fdeb3359c6bcd5f4d42391
Jonathan Sick <jsick@lsst.org>
2025-08-11 11:57:08 -0400

    Migrate next/legacy/image for Next 13

--- apps/squareone/src/components/Footer/Footer.js
+++ apps/squareone/src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import Image from 'next/image';
+import Image from 'next/legacy/image';
 import Link from 'next/link';
 
 import AgencyLogos from '@lsst-sqre/rubin-style-dictionary/assets/partner-logos/rubin-partners.png';
