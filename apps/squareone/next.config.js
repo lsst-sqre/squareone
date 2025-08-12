13ec404834aebf887d4c161efee7019244ea62b8
Jonathan Sick <jsick@lsst.org>
2025-08-12 14:19:05 -0400

    Allow use of ESM modules
    This is required for compatibility with next-mdx-remote, which is an
    ESM-only package. The typescript conversion seemed to be transpiling the
    import into a require otherwise.

--- apps/squareone/next.config.js
+++ apps/squareone/next.config.js
@@ -51,6 +51,7 @@ module.exports = (phase, { defaultConfig }) => {
     ...defaultConfig,
     experimental: {
       instrumentationHook: true,
+      esmExternals: 'loose',
     },
     publicRuntimeConfig: { sentryDsn, ...publicYamlConfig },
     serverRuntimeConfig: { sentryDsn, ...serverYamlConfig },
