e66d16d1b195e0649429d73684cb0982f231ef1c
Jonathan Sick <jsick@lsst.org>
2025-08-21 15:24:47 -0400

    Use uv for running nox
    - With the script dependencies, the nox commands can now be run without
      a pre-existing local environment with uv run noxfile.py.
    - Update github actions to setup uv
    - Use uv as the backend venv provider for nox.

--- noxfile.py
+++ noxfile.py
@@ -1,16 +1,21 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = ["nox"]
+# ///
+
 import nox
 
 # Default sessions
 nox.options.sessions = ["docs"]
 
 # Other nox defaults
-nox.options.default_venv_backend = "venv"
+nox.options.default_venv_backend = "uv"
 nox.options.reuse_existing_virtualenvs = True
 
 PIP_DEPENDENCIES = [
-    ("--upgrade", "pip"),
     ("-r", "docs/requirements.txt"),
 ]
+
 
 def _install(session: nox.Session, bin_prefix: str = "") -> None:
     """Install the application and all development dependencies into the
@@ -21,33 +26,13 @@ def _install(session: nox.Session, bin_prefix: str = "") -> None:
     # Install dev dependencies
     for deps in PIP_DEPENDENCIES:
         session.run(python, "-m", "pip", "install", *deps, external=True)
-    session.run(
-        python, "-m", "pip", "install", "nox", external=True
-    )
-
-
-@nox.session(name="venv-init")
-def init_dev(session: nox.Session) -> None:
-    """Set up a documentation venv."""
-    # Create a venv in the current directory, replacing any existing one
-    session.run("python", "-m", "venv", ".venv", "--clear")
-    _install(session, bin_prefix=".venv/bin/")
-
-    print(
-        "\nTo activate this virtual env, run:\n\n\tsource .venv/bin/activate\n"
-    )
-
-
-@nox.session(name="init", python=False)
-def init(session: nox.Session) -> None:
-    """Set up the documentation environment in the current virtual env."""
-    _install(session, bin_prefix="")
+    session.run(python, "-m", "pip", "install", "nox", external=True)
 
 
 @nox.session
 def docs(session: nox.Session) -> None:
     """Build the docs."""
-    _install(session)
+    session.install("-r", "docs/requirements.txt")
     doctree_dir = (session.cache_dir / "doctrees").absolute()
     with session.chdir("docs"):
         session.run(
@@ -68,7 +53,7 @@ def docs(session: nox.Session) -> None:
 @nox.session(name="docs-linkcheck")
 def docs_linkcheck(session: nox.Session) -> None:
     """Linkcheck the docs."""
-    _install(session)
+    session.install("-r", "docs/requirements.txt")
     doctree_dir = (session.cache_dir / "doctrees").absolute()
     with session.chdir("docs"):
         session.run(
@@ -77,9 +62,14 @@ def docs_linkcheck(session: nox.Session) -> None:
             "--keep-going",
             "-n",
             "-T",
-            "-b" "linkcheck",
+            "-blinkcheck",
             "-d",
             str(doctree_dir),
             ".",
             "./_build/html",
         )
+
+
+if __name__ == "__main__":
+    # For running standalone with `uv run noxfile.py`.
+    nox.main()
