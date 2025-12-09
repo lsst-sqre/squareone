f52b91da324f03f8d06ca7ad1d3026849b3d1d60
Jonathan Sick <jonathan@jsick.codes>
2025-12-09 16:45:54 -0500

    Migrate to modern next/Link behavior

--- apps/squareone/src/components/Header/HeaderNav.tsx
+++ apps/squareone/src/components/Header/HeaderNav.tsx
@@ -69,7 +69,8 @@ function InternalTriggerLink({ href, children }: InternalTriggerLinkProps) {
   const _isActive = href === router.pathname;
 
   return (
-    <NextLink href={href} passHref legacyBehavior>
+    <NextLink href={href}>
+      {/* @next-codemod-error This Link previously used the now removed `legacyBehavior` prop, and has a child that might not be an anchor. The codemod bailed out of lifting the child props to the Link. Check that the child component does not render an anchor, and potentially move the props manually to Link. */}
       <PrimaryNavigation.TriggerLink>{children}</PrimaryNavigation.TriggerLink>
     </NextLink>
   );
